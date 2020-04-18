e22540fc0f6991e4076b8bb5b000a56a1d11c58e
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-04-19 00:11:45 +0530

    reverting

--- code/main.py
+++ code/main.py
@@ -1,6 +1,6 @@
 import os
 import json
-import sys
+
 from azureml.core import Workspace, Experiment
 from azureml.core.authentication import ServicePrincipalAuthentication
 from azureml.pipeline.core import PipelineRun
@@ -10,135 +10,6 @@ from msrest.exceptions import AuthenticationError
 from json import JSONDecodeError
 from utils import AMLConfigurationException, AMLExperimentConfigurationException, required_parameters_provided, mask_parameter, convert_to_markdown, load_pipeline_yaml, load_runconfig_yaml, load_runconfig_python
 
-def submitRun(ws, parameters):
-    # Create experiment
-    print("::debug::Creating experiment")
-    try:
-        # Default experiment name
-        repository_name = os.environ.get("GITHUB_REPOSITORY").split("/")[-1]
-        branch_name = os.environ.get("GITHUB_REF").split("/")[-1]
-        default_experiment_name = f"{repository_name}-{branch_name}"
-
-        experiment = Experiment(
-            workspace=ws,
-            name=parameters.get("experiment_name", default_experiment_name)[:36]
-        )
-    except TypeError as exception:
-        experiment_name = parameters.get("experiment", None)
-        print(f"::error::Could not create an experiment with the specified name {experiment_name}: {exception}")
-        raise AMLExperimentConfigurationException(f"Could not create an experiment with the specified name {experiment_name}: {exception}")
-    except UserErrorException as exception:
-        experiment_name = parameters.get("experiment", None)
-        print(f"::error::Could not create an experiment with the specified name {experiment_name}: {exception}")
-        raise AMLExperimentConfigurationException(f"Could not create an experiment with the specified name {experiment_name}: {exception}")
-
-    # Loading run config
-    print("::debug::Loading run config")
-    run_config = None
-    if run_config is None:
-        # Loading run config from runconfig yaml file
-        print("::debug::Loading run config from runconfig yaml file")
-        run_config = load_runconfig_yaml(
-            runconfig_yaml_file=parameters.get("runconfig_yaml_file", "/train/run_config.yml")
-        )
-    if run_config is None:
-        # Loading run config from pipeline yaml file
-        print("::debug::Loading run config from pipeline yaml file")
-        run_config = load_pipeline_yaml(
-            workspace=ws,
-            pipeline_yaml_file=parameters.get("pipeline_yaml_file", "/train/pipeline.yml")
-        )
-    if run_config is None:
-        # Loading run config from python runconfig file
-        print("::debug::Loading run config from python runconfig file")
-        run_config = load_runconfig_python(
-            workspace=ws,
-            runconfig_python_file=parameters.get("runconfig_python_file", "/train/run_config.py"),
-            runconfig_python_function_name=parameters.get("runconfig_python_function_name", "main")
-        )
-    if run_config is None:
-        # Loading values for errors
-        pipeline_yaml_file = parameters.get("pipeline_yaml_file", "/train/pipeline.yml")
-        runconfig_yaml_file = parameters.get("runconfig_yaml_file", "/train/run_config.yml")
-        runconfig_python_file = parameters.get("runconfig_python_file", "/train/run_config.py")
-        runconfig_python_function_name = parameters.get("runconfig_python_function_name", "main")
-
-        print(f"::error::Error when loading runconfig yaml definition your repository (Path: /{runconfig_yaml_file}).")
-        print(f"::error::Error when loading pipeline yaml definition your repository (Path: /{pipeline_yaml_file}).")
-        print(f"::error::Error when loading python script or function in your repository which defines the experiment config (Script path: '/{runconfig_python_file}', Function: '{runconfig_python_function_name}()').")
-        print("::error::You have to provide a yaml definition for your run, a yaml definition of your pipeline or a python script, which returns a runconfig. Please read the documentation for more details.")
-        raise AMLExperimentConfigurationException("You have to provide a yaml definition for your run, a yaml definition of your pipeline or a python script, which returns a runconfig. Please read the documentation for more details.")
-
-    # Submit run config
-    print("::debug::Submitting experiment config")
-    try:
-        run = experiment.submit(
-            config=run_config,
-            tags=parameters.get("tags", {})
-        )
-    except AzureMLException as exception:
-        print(f"::error::Could not submit experiment config. Your script passed object of type {type(run_config)}. Object must be correctly configured and of type e.g. estimator, pipeline, etc.: {exception}")
-        raise AMLExperimentConfigurationException(f"Could not submit experiment config. Your script passed object of type {type(run_config)}. Object must be correctly configured and of type e.g. estimator, pipeline, etc.: {exception}")
-    except TypeError as exception:
-        print(f"::error::Could not submit experiment config. Your script passed object of type {type(run_config)}. Object must be correctly configured and of type e.g. estimator, pipeline, etc.: {exception}")
-        raise AMLExperimentConfigurationException(f"Could not submit experiment config. Your script passed object of type {type(run_config)}. Object must be correctly configured and of type e.g. estimator, pipeline, etc.: {exception}")
-
-    # Create outputs
-    print("::debug::Creating outputs")
-    print(f"::set-output name=experiment_name::{run.experiment.name}")
-    print(f"::set-output name=run_id::{run.id}")
-    print(f"::set-output name=run_url::{run.get_portal_url()}")
-
-    # we can publish the pipeline without waiting for run to be finished. need to verify it 
-    # Publishing pipeline
-    print("::debug::Publishing pipeline")
-    if type(run) is PipelineRun and parameters.get("publish_pipeline", False):
-        # Default pipeline name
-        repository_name = os.environ.get("GITHUB_REPOSITORY").split("/")[-1]
-        branch_name = os.environ.get("GITHUB_REF").split("/")[-1]
-        default_pipeline_name = f"{repository_name}-{branch_name}"
-
-        published_pipeline = run.publish_pipeline(
-            name=parameters.get("pipeline_name", default_pipeline_name),
-            description="Pipeline registered by GitHub Run Action",
-            version=parameters.get("pipeline_version", None),
-            continue_on_step_failure=parameters.get("pipeline_continue_on_step_failure", False)
-        )
-
-        # Creating additional outputs
-        print(f"::set-output name=published_pipeline_id::{published_pipeline.id}")
-        print(f"::set-output name=published_pipeline_status::{published_pipeline.status}")
-        print(f"::set-output name=published_pipeline_endpoint::{published_pipeline.endpoint}")
-    elif parameters.get("publish_pipeline", False):
-        print(f"::error::Could not register pipeline because you did not pass a pipeline to the action")
-
-    print("::debug::Successfully finished Azure Machine Learning Train Action")
-
-    wait_for_completion = False
-    # as we don't want to wait here, we just return the run object from here.
-    if parameters.get("wait_for_completion", True):
-        wait_for_completion = True;
-        
-    return (run,wait_for_completion);
-    
-def postRun(submittedRuns_for_wait):
-    # Waiting for run to complete
-    print("::debug::Waiting for run to complete")
-    run_pending = True
-    
-    while run_pending:
-        tempStack = submittedRuns_for_wait;
-        for run in tempStack:
-            if run.get_status() in ['Completed', 'Failed']:
-                # Creating additional outputs of finished run
-                run_metrics = run.get_metrics(recursive=True)
-                run_metrics_markdown = convert_to_markdown(run_metrics)
-                print(f"::set-output name=run_metrics::{run_metrics}")
-                print(f"::set-output name=run_metrics_markdown::{run_metrics_markdown}") 
-                submittedRuns_for_wait.remove(run);
-        if len(submittedRuns_for_wait) == 0:
-            run_pending = False
-    
 
 def main():
     # Loading input values
@@ -203,17 +74,119 @@ def main():
     except ProjectSystemException as exception:
         print(f"::error::Workspace authorizationfailed: {exception}")
         raise ProjectSystemException
-    
-    submittedRuns_for_wait = []
-    for parameter in parameters:
-        run,wait_for_completion = submitRun(ws,parameter)
-        
-        # add a list of tuple to be used later, we will use it to wait. 
-        if wait_for_completion == True:
-            submittedRuns_for_wait.append((run));
-    
-    postRun(submittedRuns_for_wait)
-    print("submission over")
+
+    # Create experiment
+    print("::debug::Creating experiment")
+    try:
+        # Default experiment name
+        repository_name = os.environ.get("GITHUB_REPOSITORY").split("/")[-1]
+        branch_name = os.environ.get("GITHUB_REF").split("/")[-1]
+        default_experiment_name = f"{repository_name}-{branch_name}"
+
+        experiment = Experiment(
+            workspace=ws,
+            name=parameters.get("experiment_name", default_experiment_name)[:36]
+        )
+    except TypeError as exception:
+        experiment_name = parameters.get("experiment", None)
+        print(f"::error::Could not create an experiment with the specified name {experiment_name}: {exception}")
+        raise AMLExperimentConfigurationException(f"Could not create an experiment with the specified name {experiment_name}: {exception}")
+    except UserErrorException as exception:
+        experiment_name = parameters.get("experiment", None)
+        print(f"::error::Could not create an experiment with the specified name {experiment_name}: {exception}")
+        raise AMLExperimentConfigurationException(f"Could not create an experiment with the specified name {experiment_name}: {exception}")
+
+    # Loading run config
+    print("::debug::Loading run config")
+    run_config = None
+    if run_config is None:
+        # Loading run config from runconfig yaml file
+        print("::debug::Loading run config from runconfig yaml file")
+        run_config = load_runconfig_yaml(
+            runconfig_yaml_file=parameters.get("runconfig_yaml_file", "code/train/run_config.yml")
+        )
+    if run_config is None:
+        # Loading run config from pipeline yaml file
+        print("::debug::Loading run config from pipeline yaml file")
+        run_config = load_pipeline_yaml(
+            workspace=ws,
+            pipeline_yaml_file=parameters.get("pipeline_yaml_file", "code/train/pipeline.yml")
+        )
+    if run_config is None:
+        # Loading run config from python runconfig file
+        print("::debug::Loading run config from python runconfig file")
+        run_config = load_runconfig_python(
+            workspace=ws,
+            runconfig_python_file=parameters.get("runconfig_python_file", "code/train/run_config.py"),
+            runconfig_python_function_name=parameters.get("runconfig_python_function_name", "main")
+        )
+    if run_config is None:
+        # Loading values for errors
+        pipeline_yaml_file = parameters.get("pipeline_yaml_file", "code/train/pipeline.yml")
+        runconfig_yaml_file = parameters.get("runconfig_yaml_file", "code/train/run_config.yml")
+        runconfig_python_file = parameters.get("runconfig_python_file", "code/train/run_config.py")
+        runconfig_python_function_name = parameters.get("runconfig_python_function_name", "main")
+
+        print(f"::error::Error when loading runconfig yaml definition your repository (Path: /{runconfig_yaml_file}).")
+        print(f"::error::Error when loading pipeline yaml definition your repository (Path: /{pipeline_yaml_file}).")
+        print(f"::error::Error when loading python script or function in your repository which defines the experiment config (Script path: '/{runconfig_python_file}', Function: '{runconfig_python_function_name}()').")
+        print("::error::You have to provide a yaml definition for your run, a yaml definition of your pipeline or a python script, which returns a runconfig. Please read the documentation for more details.")
+        raise AMLExperimentConfigurationException("You have to provide a yaml definition for your run, a yaml definition of your pipeline or a python script, which returns a runconfig. Please read the documentation for more details.")
+
+    # Submit run config
+    print("::debug::Submitting experiment config")
+    try:
+        run = experiment.submit(
+            config=run_config,
+            tags=parameters.get("tags", {})
+        )
+    except AzureMLException as exception:
+        print(f"::error::Could not submit experiment config. Your script passed object of type {type(run_config)}. Object must be correctly configured and of type e.g. estimator, pipeline, etc.: {exception}")
+        raise AMLExperimentConfigurationException(f"Could not submit experiment config. Your script passed object of type {type(run_config)}. Object must be correctly configured and of type e.g. estimator, pipeline, etc.: {exception}")
+    except TypeError as exception:
+        print(f"::error::Could not submit experiment config. Your script passed object of type {type(run_config)}. Object must be correctly configured and of type e.g. estimator, pipeline, etc.: {exception}")
+        raise AMLExperimentConfigurationException(f"Could not submit experiment config. Your script passed object of type {type(run_config)}. Object must be correctly configured and of type e.g. estimator, pipeline, etc.: {exception}")
+
+    # Create outputs
+    print("::debug::Creating outputs")
+    print(f"::set-output name=experiment_name::{run.experiment.name}")
+    print(f"::set-output name=run_id::{run.id}")
+    print(f"::set-output name=run_url::{run.get_portal_url()}")
+
+    # Waiting for run to complete
+    print("::debug::Waiting for run to complete")
+    if parameters.get("wait_for_completion", True):
+        run.wait_for_completion(show_output=True)
+
+        # Creating additional outputs of finished run
+        run_metrics = run.get_metrics(recursive=True)
+        run_metrics_markdown = convert_to_markdown(run_metrics)
+        print(f"::set-output name=run_metrics::{run_metrics}")
+        print(f"::set-output name=run_metrics_markdown::{run_metrics_markdown}")
+
+    # Publishing pipeline
+    print("::debug::Publishing pipeline")
+    if type(run) is PipelineRun and parameters.get("publish_pipeline", False):
+        # Default pipeline name
+        repository_name = os.environ.get("GITHUB_REPOSITORY").split("/")[-1]
+        branch_name = os.environ.get("GITHUB_REF").split("/")[-1]
+        default_pipeline_name = f"{repository_name}-{branch_name}"
+
+        published_pipeline = run.publish_pipeline(
+            name=parameters.get("pipeline_name", default_pipeline_name),
+            description="Pipeline registered by GitHub Run Action",
+            version=parameters.get("pipeline_version", None),
+            continue_on_step_failure=parameters.get("pipeline_continue_on_step_failure", False)
+        )
+
+        # Creating additional outputs
+        print(f"::set-output name=published_pipeline_id::{published_pipeline.id}")
+        print(f"::set-output name=published_pipeline_status::{published_pipeline.status}")
+        print(f"::set-output name=published_pipeline_endpoint::{published_pipeline.endpoint}")
+    elif parameters.get("publish_pipeline", False):
+        print(f"::error::Could not register pipeline because you did not pass a pipeline to the action")
+
+    print("::debug::Successfully finished Azure Machine Learning Train Action")
 
 
 if __name__ == "__main__":
